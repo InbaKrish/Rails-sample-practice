412daaa7b2273e5e577243aa6bf8a1a386d19e2e
Inba_Krish <inbakrish007@gmail.com>
2021-06-09 09:21:06 +0530

    Added - HTML view template from OSWD

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[ show edit update destroy ]
+  
+  def about
+  end
 
   # GET /articles or /articles.json
   def index
